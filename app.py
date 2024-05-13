583ebe729af11c3524b388fe545a46068c63ecad
micho1999 <149084650+micho1999@users.noreply.github.com>
2024-05-13 21:26:03 +0200

    Started Building Frontend. V.1

--- app.py
+++ app.py
@@ -1,31 +1,48 @@
-from flask import Flask, render_template, request
-from DB import connect_to_database, get_cursor
+#from flask import Flask, render_template, request
+#from DB import connect_to_database, get_cursor
+#app = Flask(__name__)
+
+#conn = connect_to_database()
+#cur = get_cursor(conn)
+
+#@app.route("/")
+#def home():
+#    return render_template('index.html')
+
+#@app.route("/getdata", methods=["POST"])
+#def get_data():
+#    data_choice = request.form['data-choice']
+#    cur.execute(f"SELECT * FROM {data_choice}")
+#    row_headers = [x[0] for x in cur.description]  # Dies holt die Spaltennamen
+#    results = cur.fetchall()
+#    json_data = []
+#    for result in results:
+#        json_data.append(dict(zip(row_headers, result)))
+#    return render_template('index.html', data=json_data)
+
+#if __name__ == "__main__":
+#    app.run(debug=True)
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-conn = connect_to_database()
-cur = get_cursor(conn)
+@app.route("/landingpage")
+def landingpage():
+    return render_template('landingpage.html')
 
-@app.route("/")
-def home():
-    return render_template('index.html')
+# Route für die Anmeldeseite
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
-@app.route("/getdata", methods=["POST"])
-def get_data():
-    data_choice = request.form['data-choice']
-    cur.execute(f"SELECT * FROM {data_choice}")
-    row_headers = [x[0] for x in cur.description]  # Dies holt die Spaltennamen
-    results = cur.fetchall()
-    json_data = []
-    for result in results:
-        json_data.append(dict(zip(row_headers, result)))
-    return render_template('index.html', data=json_data)
+# Neue Route für die Charts-Seite
+@app.route("/charts")
+def charts():
+    return render_template('charts.html')
+
+@app.route("/stackedchart")
+def stackedchart():
+    return render_template('stackedchart.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-#@app.route("/login")
-#def home():
- #   return render_template('HTML/loginpage.html')
-    #so immer auf die html pages linken
