e8d4a34e2e7f098f36d23607b3cf9d47ba0506cb
micho1999 <149084650+micho1999@users.noreply.github.com>
2024-07-04 14:20:31 +0200

    stacked besser gemacht

--- static/JS/overviewstacked.js
+++ static/JS/overviewstacked.js
@@ -73,6 +73,17 @@ function loadColumns(table, columnSelectId) {
     }
 }
 
+function getToolboxFeatures() {
+  return {
+    feature: {
+      saveAsImage: {},
+      restore: {},
+      dataView: { readOnly: false },
+      magicType: { type: ['line', 'bar', 'stack'] }
+    }
+  };
+}
+
 function renderChart(areaData) {
     let option = {
         tooltip: {
@@ -86,45 +97,9 @@ function renderChart(areaData) {
         toolbox: {
             feature: {
                 saveAsImage: {},
-                myDarkMode: {
-                    show: true,
-                    title: 'Dark Mode',
-                    icon: 'path://M512 0C229.23072 0 0 229.23072 0 512s229.23072 512 512 512 512-229.23072 512-512S794.76928 0 512 0z m0 938.0864c-234.24 0-426.0864-191.8464-426.0864-426.0864S277.76 85.9136 512 85.9136c55.7312 0 111.4624 11.7248 163.6864 35.0208-32.768 56.32-87.04 94.72-151.0912 105.2672-78.4896 13.7216-147.2-12.288-199.7312-55.808 0 0-12.6976 80.9472-12.6976 119.296 0 136.3968 104.7552 247.9104 239.9232 261.8368 79.872 8.2944 152.576-24.7808 198.3488-80.64 28.2624 48.64 45.568 106.752 45.568 170.3424 0 234.24-191.8464 426.0864-426.0864 426.0864z',
-                    onclick: function() {
-                        darkMode = !darkMode;
-                        updateChartAppearance();
-                    }
-                },
-                myDecalPattern: {
-                    show: true,
-                    title: 'Decal Pattern',
-                    icon: 'path://M50 250 Q 150 50 250 250 T 450 250',
-                    onclick: function() {
-                        decalPattern = !decalPattern;
-                        updateChartAppearance();
-                    }
-                },
-                myShare: {
-                    show: true,
-                    title: 'Share',
-                    icon: 'path://M864 160h-192V96H352v64H160c-35.328 0-64 28.672-64 64v576c0 35.328 28.672 64 64 64h704c35.328 0 64-28.672 64-64V224c0-35.328-28.672-64-64-64z m0 640H160V224h192v64h320v-64h192v576z m-320-320h-64v192h-192V480h-64l160-160 160 160z',
-                    onclick: function() {
-                        const url = window.location.href;
-                        navigator.clipboard.writeText(url).then(function() {
-                            alert('URL copied to clipboard');
-                        }, function(err) {
-                            console.error('Could not copy URL: ', err);
-                        });
-                    }
-                },
-                myCloseChart: {
-                    show: true,
-                    title: 'Close Chart',
-                    icon: 'path://M512 512l212.48-212.48a32 32 0 0 0-45.248-45.248L512 421.504 299.52 209.024a32 32 0 1 0-45.248 45.248L466.752 512 254.272 724.48a32 32 0 1 0 45.248 45.248L512 602.496l212.48 212.48a32 32 0 0 0 45.248-45.248L557.248 512z',
-                    onclick: function() {
-                        chartInstance.dispose();
-                    }
-                }
+                restore: {},
+      dataView: { readOnly: false },
+      magicType: { type: ['line', 'bar', 'stack'] }
             }
         },
         xAxis: {
