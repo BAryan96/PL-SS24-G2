54a2169de6f3b28305a0b81df125c17ddace09c0
micho1999 <149084650+micho1999@users.noreply.github.com>
2024-07-05 15:32:11 +0200

    heatmap viel besser gemacht

--- static/JS/overviewheatmap.js
+++ static/JS/overviewheatmap.js
@@ -1,256 +1,407 @@
-$(document).ready(function () {
-  var baseLayer = L.tileLayer(
-    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    {
-      attribution: "© OpenStreetMap contributors",
-      maxZoom: 18,
-    }
-  );
+document.addEventListener("DOMContentLoaded", () => {
+  let chartCount = 0;
+  const maxCharts = 6;
 
-  var cfg = {
-    radius: 0.2,
-    maxOpacity: 0.8,
-    scaleRadius: true,
-    useLocalExtrema: true,
-    latField: "lat",
-    lngField: "lng",
-    valueField: "count",
-    gradient: {
-      0.1: "blue",
-      0.2: "lime",
-      0.4: "yellow",
-      0.6: "orange",
-      0.8: "red",
-      1.0: "purple",
-    },
-  };
-
-  var heatmapLayer = new HeatmapOverlay(cfg);
-
-  var map = new L.Map("map", {
-    center: new L.LatLng(37.5, -117),
-    zoom: 6,
-    layers: [baseLayer],
-  });
-
-  var circleMarkerOptions = {
-    radius: 3,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.6,
-  };
-
-  function createLegend(legendElement, gradient) {
-    for (var key in gradient) {
-      var color = gradient[key];
-      var div = document.createElement("div");
-      div.innerHTML =
-        '<span style="background-color:' + color + ';"></span>' + key;
-      legendElement.appendChild(div);
-    }
+  async function fetchTables() {
+    const response = await fetch("/tables");
+    const data = await response.json();
+    return data.tables;
   }
 
-  var legendControl = L.control({ position: "bottomright" });
-
-  legendControl.onAdd = function (map) {
-    var div = L.DomUtil.create("div", "legend");
-    var gradient = cfg.gradient;
-    createLegend(div, gradient);
-    return div;
-  };
-
-  legendControl.addTo(map);
-
-  $.get("/tables", function (data) {
-    data.tables.forEach((table) => {
-      $("#tableSelect").append(`<option value="${table}">${table}</option>`);
+  async function fetchColumns(table) {
+    const response = await fetch("/columns", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `table=${encodeURIComponent(table)}`,
     });
-  });
+    const data = await response.json();
+    return data.columns;
+  }
 
-  $("#tableSelect").change(function () {
-    var table = $(this).val();
-    if (table) {
-      $.post("/columns", { table: table }, function (data) {
-        $("#columnSelect").empty().append('<option value="">None</option>');
-        data.columns.forEach((column) => {
-          $("#columnSelect").append(
-            `<option value="${column}">${column}</option>`
-          );
-        });
-      });
+  async function createSelectOptions(selectElement, options) {
+    selectElement.innerHTML = '<option value="">None</option>';
+    options.forEach((option) => {
+      const opt = document.createElement("option");
+      opt.value = option;
+      opt.text = option;
+      selectElement.appendChild(opt);
+    });
+  }
+
+  async function addChartContainer() {
+    if (chartCount >= maxCharts) {
+      alert("Maximum number of charts reached.");
+      return;
     }
-  });
 
-  $("#dataForm").submit(function (event) {
-    event.preventDefault();
-    var markerType = $("#markerTypeSelect").val();
-    var table = $("#tableSelect").val();
-    var column = $("#columnSelect").val();
-    var aggregation = $("#aggregationSelect").val();
+    const tables = await fetchTables();
 
-    if (markerType && !table && !column && !aggregation) {
-      var tables =
-        markerType === "stores"
-          ? ["stores", "stores", "stores"]
-          : ["customers", "customers", "customers"];
-      var columns =
-        markerType === "stores"
-          ? ["storeID", "longitude", "latitude"]
-          : ["customerID", "longitude", "latitude"];
+    const newChartContainer = document.createElement("div");
+    newChartContainer.className = "chart-container";
 
-      var requestData = {
-        tables: tables,
-        columns: columns,
-        chartType: "markers",
-        aggregations: ["", "", ""],
-        filters: [],
-      };
+    const closeButton = document.createElement("button");
+    closeButton.textContent = "X";
+    closeButton.className = "close-button";
+    closeButton.addEventListener("click", () => {
+      newChartContainer.remove();
+      chartCount--;
+      updateChartContainerWidth();
+    });
+    newChartContainer.appendChild(closeButton);
 
-      $.ajax({
-        url: "/getdata",
-        method: "POST",
-        contentType: "application/json",
-        data: JSON.stringify(requestData),
-        success: function (responseData) {
-          if (
-            !responseData.hasOwnProperty("x") ||
-            !responseData.hasOwnProperty("y0") ||
-            !responseData.hasOwnProperty("y1")
-          ) {
-            console.error(
-              "Data does not have the required properties:",
-              responseData
-            );
-            return;
-          }
+    const selectContainer = document.createElement("div");
+    selectContainer.className = "select-container";
 
-          var data = [];
-          for (var i = 0; i < responseData.x.length; i++) {
-            data.push({
-              id: responseData.x[i],
-              longitude: parseFloat(responseData.y0[i]),
-              latitude: parseFloat(responseData.y1[i]),
-            });
-          }
+    const markerTypeTitle = document.createElement("div");
+    markerTypeTitle.innerHTML = "<b>Markers Based On:</b>";
+    selectContainer.appendChild(markerTypeTitle);
 
-          showMarkersOnly(data);
-        },
-        error: function (error) {
-          console.log("Error:", error);
-        },
-      });
-    } else if (markerType && table && column && aggregation) {
-      var tables =
-        markerType === "stores"
-          ? ["stores", "stores", "stores"]
-          : ["customers", "customers", "customers"];
-      var columns =
-        markerType === "stores"
-          ? ["storeID", "longitude", "latitude"]
-          : ["customerID", "longitude", "latitude"];
-      tables.push(table);
-      columns.push(column);
+    const markerTypeContainer = document.createElement("div");
+    markerTypeContainer.className = "marker-type-container";
 
-      var requestData = {
-        tables: tables,
-        columns: columns,
-        chartType: "heatmap",
-        aggregations: ["", "X", "X", aggregation],
-        filters: [],
-      };
+    const createLabeledCheckbox = (labelText, value) => {
+      const label = document.createElement("label");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.value = value;
+      checkbox.name = "marker";
+      label.appendChild(checkbox);
+      label.appendChild(document.createTextNode(labelText));
+      markerTypeContainer.appendChild(label);
+    };
 
-      $.ajax({
-        url: "/getdata",
-        method: "POST",
-        contentType: "application/json",
-        data: JSON.stringify(requestData),
-        success: function (responseData) {
-          if (
-            !responseData.hasOwnProperty("x") ||
-            !responseData.hasOwnProperty("y0") ||
-            !responseData.hasOwnProperty("y1") ||
-            !responseData.hasOwnProperty("y2")
-          ) {
-            console.error(
-              "Data does not have the required properties:",
-              responseData
-            );
-            return;
-          }
+    createLabeledCheckbox("Stores", "stores");
+    createLabeledCheckbox("Customers", "customers");
 
-          var data = [];
-          for (var i = 0; i < responseData.x.length; i++) {
-            data.push({
-              id: responseData.x[i],
-              longitude: parseFloat(responseData.y0[i]),
-              latitude: parseFloat(responseData.y1[i]),
-              Aggregation: parseFloat(responseData.y2[i]),
-            });
-          }
+    selectContainer.appendChild(markerTypeContainer);
 
-          var bounds = [];
-          var heatmapPoints = [];
+    const createLabeledSelect = (labelText) => {
+      const label = document.createElement("label");
+      label.textContent = labelText;
+      const select = document.createElement("select");
+      selectContainer.appendChild(label);
+      selectContainer.appendChild(select);
+      return select;
+    };
 
-          data.forEach(function (point) {
-            var marker = L.circleMarker(
-              [point.latitude, point.longitude],
-              circleMarkerOptions
-            );
-            marker.bindPopup(
-              `<b>ID:</b> ${point.id}<br><b>Longitude:</b> ${point.longitude}<br><b>Latitude:</b> ${point.latitude}<br><b>Aggregation:</b> ${point.Aggregation}`
-            );
-            map.addLayer(marker);
-            bounds.push([point.latitude, point.longitude]);
+    const tableSelect = createLabeledSelect("Table");
+    const columnSelect = createLabeledSelect("Column");
+    const aggregationSelect = createLabeledSelect("Aggregation");
 
-            heatmapPoints.push({
-              lat: point.latitude,
-              lng: point.longitude,
-              count: point.Aggregation,
-            });
-          });
+    await createSelectOptions(tableSelect, tables);
 
-          if (bounds.length > 0) {
-            map.fitBounds(bounds);
-          }
-
-          map.addLayer(heatmapLayer);
-          heatmapLayer.setData({
-            max: Math.max(...data.map((point) => point.Aggregation)),
-            data: heatmapPoints,
-          });
-        },
-        error: function (error) {
-          console.log("Error:", error);
-        },
-      });
-    } else {
-      alert("Please select all required fields.");
-    }
-  });
-
-  function showMarkersOnly(data) {
-    var bounds = [];
-
-    data.forEach(function (point) {
-      var marker = L.circleMarker(
-        [point.latitude, point.longitude],
-        circleMarkerOptions
-      );
-      marker.bindPopup(
-        `<b>ID:</b> ${point.id}<br><b>Longitude:</b> ${point.longitude}<br><b>Latitude:</b> ${point.latitude}`
-      );
-      map.addLayer(marker);
-      bounds.push([point.latitude, point.longitude]);
+    tableSelect.addEventListener("change", async () => {
+      const selectedTable = tableSelect.value;
+      if (selectedTable) {
+        const columns = await fetchColumns(selectedTable);
+        await createSelectOptions(columnSelect, columns);
+      } else {
+        columnSelect.innerHTML = '<option value="">None</option>';
+      }
     });
 
-    if (bounds.length > 0) {
-      map.fitBounds(bounds);
+    const aggregations = [
+      "Summe",
+      "Max",
+      "Min",
+      "Anzahl",
+      "Diskrete Anzahl",
+      "Durchschnitt",
+      "Varianz",
+      "Standardabweichung",
+    ];
+    await createSelectOptions(aggregationSelect, aggregations);
+
+    const submitButton = document.createElement("button");
+    submitButton.className = "submit-button";
+    submitButton.textContent = "Submit";
+    selectContainer.appendChild(submitButton);
+
+    const mapContainer = document.createElement("div");
+    mapContainer.className = "map-container";
+
+    const mapDiv = document.createElement("div");
+    mapDiv.id = `map${chartCount}`;
+    mapDiv.className = "map";
+
+    mapContainer.appendChild(mapDiv);
+
+    newChartContainer.appendChild(selectContainer);
+    newChartContainer.appendChild(mapContainer);
+
+    document.getElementById("charts-container").appendChild(newChartContainer);
+
+    const map = L.map(mapDiv).setView([37.5, -117], 6);
+    const baseLayer = L.tileLayer(
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        attribution: "© OpenStreetMap contributors",
+        maxZoom: 18,
+      }
+    ).addTo(map);
+
+    const cfg = {
+      radius: 0.5,
+      maxOpacity: 0.8,
+      scaleRadius: true,
+      useLocalExtrema: true,
+      latField: "lat",
+      lngField: "lng",
+      valueField: "count",
+      gradient: {
+        0.1: "blue",
+        0.2: "lime",
+        0.4: "yellow",
+        0.6: "orange",
+        0.8: "red",
+        1.0: "purple",
+      },
+    };
+
+    const heatmapLayer = new HeatmapOverlay(cfg);
+
+    const circleMarkerOptions = {
+      radius: 3,
+      fillColor: "#ff7800",
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.6,
+    };
+
+    function createLegend(legendElement, gradient) {
+      for (var key in gradient) {
+        var color = gradient[key];
+        var div = document.createElement("div");
+        div.innerHTML =
+          '<span style="background-color:' + color + ';"></span>' + key;
+        legendElement.appendChild(div);
+      }
     }
 
-    if (map.hasLayer(heatmapLayer)) {
+    const legendControl = L.control({ position: "bottomright" });
+
+    legendControl.onAdd = function (map) {
+      var div = L.DomUtil.create("div", "legend");
+      var gradient = cfg.gradient;
+      createLegend(div, gradient);
+      return div;
+    };
+
+    legendControl.addTo(map);
+
+    setTimeout(() => {
+      map.invalidateSize();
+    }, 500);
+
+    let markers = L.layerGroup().addTo(map);
+
+    submitButton.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      const markerTypes = Array.from(
+        markerTypeContainer.querySelectorAll('input[name="marker"]:checked')
+      ).map((checkbox) => checkbox.value);
+      const table = tableSelect.value;
+      const column = columnSelect.value;
+      const aggregation = aggregationSelect.value;
+
+      console.log("Selected Table:", table);
+      console.log("Selected Column:", column);
+      console.log("Selected Aggregation:", aggregation);
+
+      markers.clearLayers();
       map.removeLayer(heatmapLayer);
+
+      if (markerTypes.length && !table && !column && !aggregation) {
+        const markerType = markerTypes[0];
+        const tables =
+          markerType === "stores"
+            ? ["stores", "stores", "stores"]
+            : ["customers", "customers", "customers"];
+        const columns =
+          markerType === "stores"
+            ? ["storeID", "longitude", "latitude"]
+            : ["customerID", "longitude", "latitude"];
+
+        const requestData = {
+          tables: tables,
+          columns: columns,
+          chartType: "markers",
+          aggregations: ["", "", ""],
+          filters: [],
+        };
+
+        $.ajax({
+          url: "/getdata",
+          method: "POST",
+          contentType: "application/json",
+          data: JSON.stringify(requestData),
+          success: function (responseData) {
+            console.log("Response Data:", responseData);
+            if (
+              !responseData.hasOwnProperty("x") ||
+              !responseData.hasOwnProperty("y0") ||
+              !responseData.hasOwnProperty("y1")
+            ) {
+              console.error(
+                "Data does not have the required properties:",
+                responseData
+              );
+              return;
+            }
+
+            const data = [];
+            for (let i = 0; i < responseData.x.length; i++) {
+              data.push({
+                id: responseData.x[i],
+                longitude: parseFloat(responseData.y0[i]),
+                latitude: parseFloat(responseData.y1[i]),
+              });
+            }
+
+            showMarkersOnly(data);
+          },
+          error: function (error) {
+            console.log("Error:", error);
+          },
+        });
+      } else if (markerTypes.length && table && column && aggregation) {
+        const markerType = markerTypes[0];
+        const tables =
+          markerType === "stores"
+            ? ["stores", "stores", "stores"]
+            : ["customers", "customers", "customers"];
+        const columns =
+          markerType === "stores"
+            ? ["storeID", "longitude", "latitude"]
+            : ["customerID", "longitude", "latitude"];
+        tables.push(table);
+        columns.push(column);
+
+        const requestData = {
+          tables: tables,
+          columns: columns,
+          chartType: "heatmap",
+          aggregations: ["", "X", "X", aggregation],
+          filters: [],
+        };
+
+        $.ajax({
+          url: "/getdata",
+          method: "POST",
+          contentType: "application/json",
+          data: JSON.stringify(requestData),
+          success: function (responseData) {
+            console.log("Response Data:", responseData);
+            if (
+              !responseData.hasOwnProperty("x") ||
+              !responseData.hasOwnProperty("y0") ||
+              !responseData.hasOwnProperty("y1") ||
+              !responseData.hasOwnProperty("y2")
+            ) {
+              console.error(
+                "Data does not have the required properties:",
+                responseData
+              );
+              return;
+            }
+
+            const data = [];
+            for (let i = 0; i < responseData.x.length; i++) {
+              data.push({
+                id: responseData.x[i],
+                longitude: parseFloat(responseData.y0[i]),
+                latitude: parseFloat(responseData.y1[i]),
+                Aggregation: parseFloat(responseData.y2[i]),
+              });
+            }
+
+            const bounds = [];
+            const heatmapPoints = [];
+
+            data.forEach((point) => {
+              const marker = L.circleMarker(
+                [point.latitude, point.longitude],
+                circleMarkerOptions
+              );
+              marker.bindPopup(
+                `<b>ID:</b> ${point.id}<br><b>Longitude:</b> ${point.longitude}<br><b>Latitude:</b> ${point.latitude}<br><b>Aggregation:</b> ${point.Aggregation}`
+              );
+              map.addLayer(marker);
+              bounds.push([point.latitude, point.longitude]);
+
+              heatmapPoints.push({
+                lat: point.latitude,
+                lng: point.longitude,
+                count: point.Aggregation,
+              });
+            });
+
+            if (bounds.length > 0) {
+              map.fitBounds(bounds);
+            }
+
+            map.addLayer(heatmapLayer);
+            heatmapLayer.setData({
+              max: Math.max(...data.map((point) => point.Aggregation)),
+              data: heatmapPoints,
+            });
+          },
+          error: function (error) {
+            console.log("Error:", error);
+          },
+        });
+      } else {
+        alert("Please select all required fields.");
+      }
+    });
+
+    function showMarkersOnly(data) {
+      const bounds = [];
+
+      data.forEach((point) => {
+        const marker = L.circleMarker(
+          [point.latitude, point.longitude],
+          circleMarkerOptions
+        );
+        marker.bindPopup(
+          `<b>ID:</b> ${point.id}<br><b>Longitude:</b> ${point.longitude}<br><b>Latitude:</b> ${point.latitude}`
+        );
+        map.addLayer(marker);
+        bounds.push([point.latitude, point.longitude]);
+      });
+
+      if (bounds.length > 0) {
+        map.fitBounds(bounds);
+      }
+
+      if (map.hasLayer(heatmapLayer)) {
+        map.removeLayer(heatmapLayer);
+      }
     }
+
+    chartCount++;
+    updateChartContainerWidth();
+  }
+
+  document
+    .getElementById("add-chart-button")
+    .addEventListener("click", addChartContainer);
+
+  addChartContainer();
+
+  function updateChartContainerWidth() {
+    const chartContainers = document.querySelectorAll(".chart-container");
+    chartContainers.forEach((container, index) => {
+      container.style.width = "48%";
+      if (index % 2 !== 0) {
+        container.style.marginRight = "0";
+      } else {
+        container.style.marginRight = "2%";
+      }
+    });
   }
 });
